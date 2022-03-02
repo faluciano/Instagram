8befbce4f666dd2a5f23ade33fb445d1721a3767
Felix Luciano <felixluciano.a@gmail.com>
2022-03-01 19:49:20 -0500

    Post can now be submitted

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -29,8 +33,38 @@ public class MainActivity extends AppCompatActivity {
         btnPicture = findViewById(R.id.btnPicture);
         ivPicture = findViewById(R.id.ivPicture);
         btnSubmit = findViewById(R.id.btnSubmit);
-        
-        queryPosts();
+
+        btnSubmit.setOnClickListener(v -> {
+            String description = etDescription.getText().toString();
+            if (description.isEmpty()){
+                Toast.makeText(MainActivity.this,"Description can't be empty", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            savePost(description,currentUser);
+        });
+
+        //queryPosts();
+    }
+
+    private void savePost(String description, ParseUser currentUser) {
+        Post post = new Post();
+        post.setDescription(description);
+//        post.setImage();
+        post.setUser(currentUser);
+
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e!=null){
+                    Log.e(TAG,"Save failed",e);
+                    Toast.makeText(MainActivity.this,"Error while saving", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Log.i(TAG,"saved post");
+                etDescription.setText("");
+            }
+        });
     }
 
     private void queryPosts() {
