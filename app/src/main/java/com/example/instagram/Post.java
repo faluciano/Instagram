5b0de6458003fd76a20e7bb71b1932c66c2b5a31
Felix Luciano <felixluciano.a@gmail.com>
2022-03-08 02:29:30 -0500

    Added ui change when post is liked as well as like counter and date created text

--- app/src/main/java/com/example/instagram/Post.java
+++ app/src/main/java/com/example/instagram/Post.java
@@ -10,6 +10,7 @@ public class Post extends ParseObject {
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_LIKES = "likes";
 
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
@@ -34,4 +35,8 @@ public class Post extends ParseObject {
     public void setUser(ParseUser user){
         put(KEY_USER,user);
     }
+
+    public int getLikes(){ return getInt(KEY_LIKES);}
+
+    public void setLikes(int likes_count){put(KEY_LIKES,likes_count);}
 }
--- app/src/main/java/com/example/instagram/PostsAdapter.java
+++ app/src/main/java/com/example/instagram/PostsAdapter.java
@@ -8,12 +8,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import org.json.JSONArray;
 
 import java.util.List;
 
@@ -61,23 +69,70 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
         private TextView tvUsername;
         private ImageView ivImage;
         private TextView tvDescription;
+        private ImageView ivLike;
+        private TextView tvLikes;
+        private TextView tvDate;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvUsername=itemView.findViewById(R.id.tvUsername);
             ivImage = itemView.findViewById(R.id.ivImage);
             tvDescription = itemView.findViewById(R.id.tvDescription);
+            ivLike = itemView.findViewById(R.id.ivLike);
+            tvLikes = itemView.findViewById(R.id.tvLikes);
+            tvDate = itemView.findViewById(R.id.tvDate);
         }
 
         public void bind(Post post) {
             String username = post.getUser().getUsername();
             tvDescription.setText(Html.fromHtml("<b>@" + username + "</b> "+post.getDescription()));
             tvUsername.setText(username);
+            tvDate.setText(post.getCreatedAt().toString());
             ParseFile image = post.getImage();
             if (image != null ){
                 Glide.with(context)
                         .load(image.getUrl())
                         .into(ivImage);
+            }
+            tvLikes.setText(post.getLikes()+" likes");
+            ivLike.setOnClickListener(v->{
+                try {
+                    List<String> likedPosts = ParseUser.getCurrentUser().fetch().getList("likedPosts");
+                    if(!likedPosts.contains(post.getObjectId())){
+                        int new_likes = post.getLikes()+1;
+                        post.setLikes(new_likes);
+                        post.saveInBackground(e -> {
+                            if (e!=null){
+                                Log.e(TAG,"Save failed",e);
+                                Toast.makeText(context,"Error while saving", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                            Log.i(TAG,"saved post");
+                            tvLikes.setText(new_likes+" likes");
+                            ivLike.setImageResource(R.drawable.ufi_heart_active);
+                            likedPosts.add(post.getObjectId());
+                            ParseUser.getCurrentUser().put("likedPosts",likedPosts);
+                            ParseUser.getCurrentUser().saveInBackground(err->{
+                                if(err != null){
+                                    Log.e(TAG,"User Like save failed",e);
+                                    return;
+                                }
+                                Log.i(TAG,"User like saved");
+                            });
+                        });
+                    }
+                } catch (ParseException parseException) {
+                    parseException.printStackTrace();
+                }
+
+            });
+            try {
+                if (ParseUser.getCurrentUser().fetch().getList("likedPosts").contains(post.getObjectId())) {
+                    ivLike.setImageResource(R.drawable.ufi_heart_active);
+                    ivLike.setOnClickListener(null);
+                }
+            } catch (ParseException parseException){
+                parseException.printStackTrace();
             }
         }
     }
